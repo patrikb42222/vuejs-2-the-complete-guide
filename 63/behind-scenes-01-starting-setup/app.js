3dbf83b4e875827efb9536ba772cce4dd124f02f
patrikb42 <patrik@bedeai.com>
2024-07-20 22:17:02 +0200

    docs: Vue behind the scenes

--- 63/behind-scenes-01-starting-setup/app.js
+++ 63/behind-scenes-01-starting-setup/app.js
@@ -6,16 +6,44 @@ const app = Vue.createApp({
     };
   },
   methods: {
-    saveInput(event) {
+    /*saveInput(event) {
       this.currentUserInput = event.target.value;
-    },
+    },*/
     setText() {
-      this.message = this.currentUserInput;
+      //this.message = this.currentUserInput;
+      console.log(this.$refs.userText.value);
+      this.message = this.$refs.userText.value;
     },
+  },
+  beforeCreate() {
+    console.log("Before create");
+  },
+  created() {
+    console.log("Created");
+  },
+  beforeMount() {
+    console.log("Before mount");
+  },
+  mounted() {
+    console.log("Mounted");
+  },
+  beforeUpdate() {
+    console.log("Before update");
+  },
+  updated() {
+    console.log("Updated");
+  },
+  beforeUnmount() {
+    console.log("Before unmount");
+  },
+  unmounted() {
+    console.log("Unmounted");
   },
 });
 
 app.mount("#app");
+
+app.unmount();
 
 const app2 = Vue.createApp({
   template: `
