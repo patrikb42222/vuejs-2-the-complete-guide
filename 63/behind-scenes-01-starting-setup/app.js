860113eb02d100ea794264088fd0871daa7d304f
patrikb42 <patrik@bedeai.com>
2024-07-20 21:49:16 +0200

    docs: Multiple apps

---
+++ 63/behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,31 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: "",
+      message: "Vue is great!",
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      this.message = this.currentUserInput;
+    },
+  },
+});
+
+app.mount("#app");
+
+const app2 = Vue.createApp({
+  template: `
+  <p>{{ favoriteMeal }}</p>
+  `,
+  data() {
+    return {
+      favoriteMeal: "Pizza",
+    };
+  },
+});
+
+app2.mount("#app2");
