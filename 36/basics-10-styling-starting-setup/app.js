72fd6f50840a0c54f0bc982f505e0089ccbac9ef
patrikb42 <patrik@bedeai.com>
2024-07-20 17:21:54 +0200

    docs: Inline styling

---
+++ 36/basics-10-styling-starting-setup/app.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+  data: () => {
+    return {
+      boxASelected: false,
+      boxBSelected: false,
+      boxCSelected: false,
+    };
+  },
+  methods: {
+    boxSelected(box) {
+      console.log("boxSelected", box);
+      if (box === "A") this.boxASelected = true;
+      if (box === "B") this.boxBSelected = true;
+      if (box === "C") this.boxCSelected = true;
+    },
+  },
+});
+
+app.mount("#styling");
