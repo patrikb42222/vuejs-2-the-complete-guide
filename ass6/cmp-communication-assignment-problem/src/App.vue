94ae941e3f78e377a6259c02523fbbaca3846cc2
patrikb42 <patrik@bedeai.com>
2024-07-21 14:38:43 +0200

    docs: Ass 6

---
+++ ass6/cmp-communication-assignment-problem/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <active-user :name="name" :age="age"></active-user>
+  <user-data @submit="onSubmit"></user-data>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      age: null,
+    };
+  },
+  methods: {
+    onSubmit(name, age) {
+      console.log(name, age);
+      this.name = name;
+      this.age = Number(age);
+    },
+  },
+};
+</script>
---
+++ ass6/cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,32 @@
+<template>
+  <form @submit.prevent="onSubmitForm">
+    <div>
+      <label>Name</label>
+      <input v-model="name" type="text" />
+    </div>
+    <div>
+      <label>Age</label>
+      <input v-model="age" type="text" />
+    </div>
+    <div>
+      <button @click="submit">submit</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      age: "",
+    };
+  },
+  emits: ["submit"],
+  methods: {
+    onSubmitForm() {
+      this.$emit("submit", this.name, this.age);
+    },
+  },
+};
+</script>
