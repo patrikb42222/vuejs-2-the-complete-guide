eb811ec803519b15e34a339e6bfe0968fe8141e5
patrikb42 <patrik@bedeai.com>
2024-07-21 16:04:12 +0200

    docs: Scoped slots

---
+++ 112/cmp-adv-01-starting-setup/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+    <course-goals v-slot:default="slotProps">
+      <h2>{{ slotProps["goal"] }}</h2>
+    </course-goals>
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader.vue";
+import UserInfo from "./components/UserInfo.vue";
+import BadgeList from "./components/BadgeList.vue";
+import CourseGoals from "./components/CourseGoals.vue";
+
+export default {
+  components: {
+    TheHeader,
+    UserInfo,
+    BadgeList,
+    CourseGoals,
+  },
+  data() {
+    return {
+      activeUser: {
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
