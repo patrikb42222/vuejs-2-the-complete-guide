5dcc88ac5c81fceff5945b5245eacd9d92f828e1
patrikb42 <patrik@bedeai.com>
2024-07-22 20:36:46 +0200

    docs: http requests

---
+++ 154/http-01-starting-setup/src/components/survey/UserExperiences.vue
@@ -0,0 +1,82 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Submitted Experiences</h2>
+      <div>
+        <base-button @click="loadExperiences"
+          >Load Submitted Experiences</base-button
+        >
+      </div>
+      <p v-if="isLoading">Loading...</p>
+      <p v-else-if="error">{{ this.error }}</p>
+      <p v-else-if="!results || results.length == 0">
+        No stored experiences found.
+      </p>
+      <ul v-else>
+        <survey-result
+          v-for="result in results"
+          :key="result.id"
+          :name="result.name"
+          :rating="result.rating"
+        ></survey-result>
+      </ul>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import SurveyResult from './SurveyResult.vue';
+
+export default {
+  data() {
+    return {
+      results: [],
+      isLoading: false,
+      error: null,
+    };
+  },
+  components: {
+    SurveyResult,
+  },
+  methods: {
+    loadExperiences() {
+      this.isLoading = true;
+      fetch(
+        'https://vue-http-demo-a1878-default-rtdb.firebaseio.com/surveys.json'
+      )
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then((data) => {
+          this.isLoading = false;
+          const results = [];
+          for (const id in data) {
+            results.push({
+              id: id,
+              name: data[id].name,
+              rating: data[id].rating,
+            });
+          }
+          this.results = results;
+        })
+        .catch(() => {
+          this.isLoading = false;
+          this.error = 'Failed to fetch data - please try again later';
+        });
+    },
+  },
+  mounted() {
+    this.loadExperiences();
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
