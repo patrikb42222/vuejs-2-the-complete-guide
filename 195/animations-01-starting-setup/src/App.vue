0ab56effd0cbf016c03073b2f85a2be4cfb092ba
patrikb42 <patrik@bedeai.com>
2024-07-25 20:35:12 +0200

    docs: Animations

---
+++ 195/animations-01-starting-setup/src/App.vue
@@ -0,0 +1,193 @@
+<template>
+  <div class="container">
+    <users-list></users-list>
+  </div>
+  <div class="container">
+    <div class="block" :class="{ animate: animatedBlock }"></div>
+    <button @click="animateBlock">Animate</button>
+  </div>
+  <div class="container">
+    <transition :css="false"
+    @enter="enter"
+    @leave="leave"
+    @enter-cancelled="enterCancelled"
+    @leave-cancelled="leaveCancelled"
+    >
+      <p v-if="paraIsVisible">This is only sometimes visible</p>
+    </transition>
+    <button @click="toggleParagraph">Toggle Paragraph</button>
+  </div>
+  <div class="container">
+    <transition name="fade-button" mode="out-in">
+      <button @click="showUsers" v-if="!usersAreVisible">Show Users</button>
+      <button @click="hideUsers" v-else>Hide Users</button>
+    </transition>
+  </div>
+  <base-modal @close="hideDialog" :open="dialogIsVisible">
+    <p>This is a test dialog!</p>
+    <button @click="hideDialog">Close it!</button>
+  </base-modal>
+  <div class="container">
+    <button @click="showDialog">Show Dialog</button>
+  </div>
+</template>  
+
+<script>
+import UsersList from './components/UsersList.vue';
+
+export default {
+  components: {
+    UsersList,
+  },
+  data() {
+    return {
+      dialogIsVisible: false,
+      animatedBlock: false,
+      paraIsVisible: false,
+      usersAreVisible: false,
+
+      paraOpacity: 0,
+      enterInterval: null,
+      leaveInterval: null
+     };
+  },
+  methods: {
+    showDialog() {
+      this.dialogIsVisible = true;
+    },
+    toggleParagraph() {
+      this.paraIsVisible = !this.paraIsVisible
+    },
+    hideDialog() {
+      this.dialogIsVisible = false;
+    },
+    animateBlock() {
+      this.animatedBlock = true;
+    },
+    beforeEnter(el) {
+      console.log("before enter")
+      console.log(el)
+      el.style.opacity = 0;
+    },
+    enter(el, done) {
+      console.log("enter")
+      this.enterInterval = setInterval(() => {
+        if (this.paraOpacity < 1) {
+          this.paraOpacity+=0.01;
+          el.style.opacity = this.paraOpacity;
+        }
+        else {
+          clearInterval(this.enterInterval);
+          done()
+        }
+      })
+    },
+    leave(el, done) {
+      console.log("leave")
+      this.leaveInterval = setInterval(() => {
+        el.style.opacity = this.paraOpacity;
+        if (this.paraOpacity > 0) {
+          this.paraOpacity-=0.01;
+          el.style.opacity = this.paraOpacity;
+        }
+        else {
+          clearInterval(this.leaveInterval);
+          done()
+        }
+      })
+    },
+    enterCancelled() {
+      clearInterval(this.enterInterval);
+    },
+    leaveCancelled() {
+      clearInterval(this.leaveInterval);
+    },
+    showUsers() {
+      this.usersAreVisible = true;
+    },
+    hideUsers() {
+      this.usersAreVisible = false;
+    }
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 0;
+}
+button {
+  font: inherit;
+  padding: 0.5rem 2rem;
+  border: 1px solid #810032;
+  border-radius: 30px;
+  background-color: #810032;
+  color: white;
+  cursor: pointer;
+}
+button:hover,
+button:active {
+  background-color: #a80b48;
+  border-color: #a80b48;
+}
+.block {
+  width: 8rem;
+  height: 8rem;
+  background-color: #290033;
+  margin-bottom: 2rem;
+  /* transition: transform 0.3s ease-out; */
+}
+.container {
+  max-width: 40rem;
+  margin: 2rem auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 2rem;
+  border: 2px solid #ccc;
+  border-radius: 12px;
+}
+
+.animate {
+  /* transform: translateX(-150px); */
+  animation: slide-scale 0.3s ease-in-out forwards;
+}
+
+.fade-button-enter-from,
+.fade-button-leave-to {
+  opacity: 0;
+}
+
+.fade-button-leave-active {
+  transition:  opacity 0.3s ease-out;
+}
+.fade-button-enter-active {
+  transition:  opacity 0.3s ease-in;
+}
+
+.fade-button-enter-to,
+.fade-button-leave-from {
+  opacity: 1;
+}
+
+@keyframes slide-scale {
+  0% {
+    fransform: translateX(0) scale(1);
+  }
+
+  70% {
+    transform: translateX(-120px) scale(1.1);
+  }
+
+  100% {
+    transform: translateX(-150px) scale(1);
+  }
+}
+</style>
