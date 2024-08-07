b33eb713e1ef3650526e83487f78bd3845cb9427
patrikb42 <patrik@bedeai.com>
2024-08-07 17:41:00 +0200

    docs: Coach project initial commit

---
+++ 238/main-prj-01-starting-setup/src/store/modules/coaches/getters.js
@@ -0,0 +1,15 @@
+export default {
+    coaches(state) {
+        return state.coaches
+    },
+    hasCoaches(state) {
+        return state.coaches && state.coaches.length > 0
+    },
+    isCoach(state, getters, rootState, rootGetters) {
+        const coaches = getters.coaches
+        const userId = rootGetters.userId
+        return coaches.some((coach) => {
+            return coach.id === userId
+        })
+    }
+}
